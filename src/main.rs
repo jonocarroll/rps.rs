23eaba60e491139d25e4a072f081481ea6ef1328
Jonathan Carroll <github@jcarroll.com.au>
2023-06-23 18:49:16 +0930

    working game

---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use clap::Parser;
+use rand::prelude::*;
+use std::cmp::*;
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Parser)]
+#[command(name = "rps")]
+#[command(about = "Rock, Paper, Scissors Game", long_about = None)]
+struct Args {
+    #[arg()]
+    throw: Option<Throw>,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd)]
+enum Throw {
+    Rock,
+    Paper,
+    Scissors,
+    Invalid,
+}
+
+impl Ord for Throw {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use Throw::*;
+        match (*self, *other) {
+            (Rock, Paper) => Ordering::Less,
+            (Rock, Scissors) => Ordering::Greater,
+            (Rock, Rock) => Ordering::Equal,
+            (Paper, Rock) => Ordering::Greater,
+            (Paper, Scissors) => Ordering::Less,
+            (Paper, Paper) => Ordering::Equal,
+            (Scissors, Rock) => Ordering::Less,
+            (Scissors, Paper) => Ordering::Greater,
+            (Scissors, Scissors) => Ordering::Equal,
+            (_, _) => Ordering::Equal,
+        }
+    }
+}
+
+impl fmt::Display for Throw {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let res = match self {
+            Self::Rock => "Rock 🪨",
+            Self::Paper => "Paper 🧻",
+            Self::Scissors => "Scissors ✂️",
+            Self::Invalid => "Invalid Input 🐛",
+        };
+        write!(f, "{}", res)
+    }
+}
+
+#[derive(Debug)]
+enum GameResult {
+    YouWin,
+    YouLose,
+    Tie,
+}
+
+impl fmt::Display for GameResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let res = match self {
+            Self::YouWin => "You win, congrats! 🎉",
+            Self::YouLose => "Sorry, you lose 😿",
+            Self::Tie => "It's a tie! 👔",
+        };
+        write!(f, "{}", res)
+    }
+}
+
+impl FromStr for Throw {
+    type Err = std::string::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let res = match &s.to_lowercase().trim_end()[..] {
+            "rock" => Throw::Rock,
+            "paper" => Throw::Paper,
+            "scissors" => Throw::Scissors,
+            _ => Throw::Invalid,
+        };
+        Ok(res)
+    }
+}
+
+impl Throw {
+    fn user(self) -> Self {
+        println!("You threw... {}", self);
+        if matches!(self, Throw::Invalid) {
+            eprintln!("I'm not playing with you now.");
+            std::process::exit(1)
+        }
+        self
+    }
+
+    fn computer() -> Self {
+        let mut rng = rand::thread_rng();
+        let r = [Throw::Rock, Throw::Paper, Throw::Scissors]
+            .choose(&mut rng)
+            .clone();
+        println!("Computer throws {}", r.unwrap());
+        r.unwrap().to_owned()
+    }
+}
+
+fn play(a: Throw, b: Throw) -> GameResult {
+    let result = match a.cmp(&b) {
+        Ordering::Equal => GameResult::Tie,
+        Ordering::Greater => GameResult::YouWin,
+        Ordering::Less => GameResult::YouLose,
+    };
+
+    println!("Result: {result}");
+
+    result
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.throw {
+        Some(val) => {
+            print!("\x1B[2J\x1B[1;1H"); // clear screen
+            println!("Let's play 🪨🧻✂️!");
+            let user = val.user();
+            let computer = Throw::computer();
+            play(user, computer);
+            std::process::exit(0)
+        }
+        None => loop {
+            print!("\x1B[2J\x1B[1;1H"); // clear screen
+            println!("Let's play! 🪨🧻✂️");
+            println!("What do you throw?");
+            let mut user_input = String::new();
+            std::io::stdin().read_line(&mut user_input).ok();
+            let user = Throw::from_str(user_input.as_str()).unwrap().user();
+            let computer = Throw::computer();
+            play(user, computer);
+            println!("Press ENTER to play again, or anything else to quit");
+            user_input = "".to_string();
+            std::io::stdin().read_line(&mut user_input).ok();
+            match &user_input.to_lowercase().trim_end()[..] {
+                "" => (),
+                _ => std::process::exit(0),
+            }
+        },
+    };
+}
